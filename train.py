46b6eaeb76e6755094d27de9f7e58d75d1930090
Aakash Chaudhary <133581067+aakashchaudhary2412@users.noreply.github.com>
2024-02-15 13:42:12 +0530

    Update train.py

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ import yaml
 from torch.optim import lr_scheduler
 from tqdm import tqdm
 
-FILE = Path(train_data-20240215T071402Z-001.zip).resolve()
+FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
